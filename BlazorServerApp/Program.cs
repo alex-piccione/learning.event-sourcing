232ef3bf445704b9076000fe9482adb60a68808b
Alex <alessandro.piccione.75@gmail.com>
2022-05-21 15:31:55 +0100

    load api url from configuration

--- BlazorServerApp/Program.cs
+++ BlazorServerApp/Program.cs
@@ -2,27 +2,28 @@ using BlazorServerApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var webApiUrl = configuration["web api url"];
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient("web api",
-    client => client.BaseAddress = new Uri("http://localhost:51010")
+    client => client.BaseAddress = new Uri(webApiUrl)
 );
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<SettingsService>();
 
 var app = builder.Build();
 
-var webApiUrl = app.Configuration["web api url"];
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
-
 
 app.UseStaticFiles();
 
