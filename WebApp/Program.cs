e42d696a796b54c9cc7fb5ff825ed70a826eea37
Alex <alessandro.piccione.75@gmail.com>
2022-05-21 14:19:33 +0100

    Add notes about impossibility to debug.

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -2,14 +2,21 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebApp;
 
+
+// Breackpoints are not hit
+// Console.WriteLine has no effect
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var webApiUrl = new ConfigurationManager()["Web API URL"];
 
-Console.WriteLine("aaaaaaaaaaaaaaaa")
+// broser Console shows an error because the URL is null.
+// without breakpoin an exception handling is impossible to develop the application... switch to React.
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(webApiUrl) });
 
 await builder.Build().RunAsync();
+
